fbad0680e4e65390b5fcda0465a4a6b437234fa5
Harshita Mehta <mharshita687@gmail.com>
2025-11-06 21:45:08 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum;
+cout<<"enter first number:";
+cin>>a;
+cout<<"enter second number:";
+cin>>b;
+
+sum=a+b;
+
+cout<<"sum="<<sum<<endl;
+return 0;
+}
